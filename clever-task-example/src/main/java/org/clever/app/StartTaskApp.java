70effba4483c631d07d5f33af9d10eaa40751d1c
lizw-2015 <1183409807@qq.com>
2025-07-07 10:18:58 +0800

    feat(task): 添加 clever-task-example模块
    - 新增 clever-task-example 模块，用于演示分布式定时任务功能
    - 添加了基本的项目结构和配置文件
    - 实现了一个简单的定时任务示例
    - 配置了日志和数据库连接

---
+++ clever-task-example/src/main/java/org/clever/app/StartTaskApp.java
@@ -0,0 +1,15 @@
+package org.clever.app;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 作者：lizw <br/>
+ * 创建时间：2022/07/16 21:25 <br/>
+ */
+@Slf4j
+public class StartTaskApp {
+    public static void main(String[] args) {
+        System.setProperty("polyglot.engine.WarnInterpreterOnly", "false");
+        AppBootstrap.start(args);
+    }
+}
